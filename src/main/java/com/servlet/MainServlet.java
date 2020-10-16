f5fb7b071bd149bec399a32890981d3d5de70737
SnejoK <Bogdan.snejok@gmail.com>
2020-10-16 15:37:44 +0300

    create pattern

--- src/main/java/com/servlet/MainServlet.java
+++ src/main/java/com/servlet/MainServlet.java
@@ -3,6 +3,7 @@ package com.servlet;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,12 +13,15 @@ public class MainServlet extends HttpServlet{
     
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String idParametr = req.getParameter("id");
-        if (idParametr != null) {
-                req.getRequestDispatcher("website/init.jsp").forward(req, resp);
-        } else {
-            req.getRequestDispatcher("website/mainpage.jsp").forward(req, resp);
+        Cookie[] cookies = req.getCookies();
+        for(Cookie c : cookies){
+            System.out.println(c.getName() + "=" + c.getValue());
         }
+//        if (idParametr != null) {
+//                req.getRequestDispatcher("website/init.jsp").forward(req, resp);
+//        } else {
+//            req.getRequestDispatcher("website/mainpage.jsp").forward(req, resp);
+//        }
     }
     
     
